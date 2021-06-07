404387fb5e380dbe6ed43993eab80cb3d8ca9687
pstojanovichtec <petar.stojanovic@htecgroup.com>
2021-06-07 12:20:52 +0200

    removed unnecessary backdorp

--- src/components/DateRangePickerWrapper.tsx
+++ src/components/DateRangePickerWrapper.tsx
@@ -17,16 +17,6 @@ const useStyles = makeStyles(() => ({
     position: 'relative',
     zIndex: 1,
   },
-  dateRangeBackdrop: {
-    position: 'fixed',
-    height: '100vh',
-    width: '100vw',
-    bottom: 0,
-    zIndex: 0,
-    right: 0,
-    left: 0,
-    top: 0,
-  },
 }));
 
 export interface DateRangePickerWrapperProps {
@@ -47,36 +37,13 @@ const DateRangePickerWrapper: React.FunctionComponent<DateRangePickerWrapperProp
   const classes = useStyles();
 
   const {
-    closeOnClickOutside,
     wrapperClassName,
-    toggle,
-    open,
   } = props;
-
-  const handleToggle = () => {
-    if (closeOnClickOutside === false) {
-      return;
-    }
-
-    toggle();
-  };
-
-  const handleKeyPress = (event: any) => event?.key === 'Escape' && handleToggle();
 
   const wrapperClasses = classNames(classes.dateRangePicker, wrapperClassName);
 
   return (
     <div className={classes.dateRangePickerContainer}>
-      {
-        open && (
-          <div
-            className={classes.dateRangeBackdrop}
-            onKeyPress={handleKeyPress}
-            onClick={handleToggle}
-          />
-        )
-      }
-
       <div className={wrapperClasses}>
         <DateRangePicker {...props} />
       </div>
